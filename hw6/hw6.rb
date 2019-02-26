ef75c8bfd65d789c05375b7cf636ec607dace0f2
Payton Kennedy <46946614+pkennedy1999@users.noreply.github.com>
2019-02-25 20:06:05 -0500

    Delete hw6.rb

--- hw6/hw6.rb
+++
@@ -1,7 +0,0 @@
-str = gets #input string from standard in
-
-str = str.gsub(/sh$/, 'ti')  #replaces 'sh' at end of words with 'ti'
-str = str.gsub(/^[f]/, 'gh') #replaces 'f' at beginning of words with 'gh'
-str = str.gsub(/(?!^)i(?!$)/, 'o') #replaces 'i' in middle of words with 'o'
-
-puts str #prints the string with the replacemets
